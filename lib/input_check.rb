129e396d2b53b6f0dd47f152fb98463f564ec62c
shen-sat <shensat18@gmail.com>
2017-12-30 21:45:02 +0000

    changes

--- lib/input_check.rb
+++ lib/input_check.rb
@@ -1,5 +1,5 @@
 class InputCheck
-	attr_reader :plateau_x, :plateau_y
+	attr_reader :plateau_x, :plateau_y, :start_pos_x, :start_pos_y, :start_pos_direction
 
 	def plateau_check(string)
 		if !string.match(/^\d\s\d$/)
@@ -7,6 +7,22 @@ class InputCheck
 		else
 			@plateau_x = string.split(" ")[0].to_i
 			@plateau_y = string.split(" ")[1].to_i
+		end
+	end
+
+	def start_pos_check(string)
+		if !string.match(/^\d\s\d\s[NESWnesw]$/)
+			raise "incorrect start position"
+		else
+			@start_pos_x = string.split(" ")[0].to_i
+			@start_pos_y = string.split(" ")[1].to_i
+			@start_pos_direction = string.split(" ")[2].upcase
+		end
+	end
+
+	def instructions_check(string)
+		if !string.match(/A-Z+/)
+			raise "incorrect instructions"
 		end
 	end
 
--- spec/input_check_spec.rb
+++ spec/input_check_spec.rb
@@ -29,3 +29,58 @@ describe 'plateau input check' do
 	end
 	
 end
+
+describe 'starting position input check' do 
+	
+	it 'should not accept input with no spaces' do 
+		input_check = InputCheck.new
+		expect{input_check.start_pos_check("12N")}.to raise_error("incorrect start position")
+	end
+
+	it 'should only accept only accept two numbers followed by a letter' do 
+		input_check = InputCheck.new
+		expect{input_check.start_pos_check("1 N")}.to raise_error("incorrect start position")
+		expect{input_check.start_pos_check("1 2 N N")}.to raise_error("incorrect start position")
+		expect{input_check.start_pos_check("1 2 1")}.to raise_error("incorrect start position")
+		expect{input_check.start_pos_check("N N N")}.to raise_error("incorrect start position")
+	end
+
+	it 'should only accept letters N, E, S, W as the last character' do 
+		input_check = InputCheck.new
+		expect{input_check.start_pos_check("1 2 A")}.to raise_error("incorrect start position")
+	end
+	
+	it 'should assign start_pos_x to first character for correct input' do 
+		input_check = InputCheck.new
+		input_check.start_pos_check("1 2 N")
+		expect(input_check.start_pos_x).to eq(1)
+	end
+
+	it 'should assign start_pos_y to second character for correct input' do 
+		input_check = InputCheck.new
+		input_check.start_pos_check("1 2 N")
+		expect(input_check.start_pos_y).to eq(2)
+	end
+
+	it 'should assign start_pos_direction to third character for correct input' do 
+		input_check = InputCheck.new
+		input_check.start_pos_check("1 2 N")
+		expect(input_check.start_pos_direction).to eq("N")
+	end
+
+	it 'should accept small caps' do 
+		input_check = InputCheck.new
+		input_check.start_pos_check("1 2 n")
+		expect(input_check.start_pos_direction).to eq("N")
+	end
+
+end
+
+describe 'instruction input check' do
+
+	it 'should only accept letters LMR with no spaces anywhere' do 
+		input_check = InputCheck.new
+		expect{input_check.instructions_check("L M R")}.to raise_error("incorrect instructions")
+
+	end  
+end
